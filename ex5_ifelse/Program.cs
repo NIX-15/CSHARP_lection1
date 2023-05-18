0c210338dbac084091b584b042cf54b8ac3311ee
Nikita <nix15@bk.ru>
2023-05-18 22:40:16 +0300

    If/Else condition

---
+++ ex5_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input YOUR name!");
+string username = Console.ReadLine();
+if (username.ToLower() == "nikita")
+{
+    Console.WriteLine("Hello, Nikita!");
+    Console.WriteLine("Glad to see you again!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
